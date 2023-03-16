33c74a48b9c5a59b6934f4717d1bdfb6482ada3f
JohannTabares <al02972975@tecmilenio.mx>
2023-03-15 21:38:11 -0600

    Iniciando el crud

---
+++ app/Http/Controllers/HeroesController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Heroes;
+use Illuminate\Http\Request;
+
+class HeroesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return view('Heroes.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('Heroes.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        //$datosHeroes = request()->all();
+        $datosHeroes = request()->except('_token');
+
+        if($request->hasFile('Foto')){
+            $datosHeroes['Foto']=$request->file('Foto')->store('uploads','public');
+            
+        }
+
+        Heroes::insert($datosHeroes);
+
+        return response()->json($datosHeroes);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Heroes  $heroes
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Heroes $heroes)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Heroes  $heroes
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Heroes $heroes)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Heroes  $heroes
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Heroes $heroes)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Heroes  $heroes
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Heroes $heroes)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_03_14_044126_create_heroes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('heroes', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('Nombre');
+            $table->string('Nombre_Real');
+            $table->string('Apellido');
+            $table->string('Localizacion');
+            $table->string('Foto');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('heroes');
+    }
+};
